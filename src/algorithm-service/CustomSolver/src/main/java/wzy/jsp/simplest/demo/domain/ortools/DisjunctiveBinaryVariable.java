a27ad35ba0993b065c7c7db74d826e9bf244316e
WANG Zhenyang <wzn7szh@bosch.com>
2020-09-18 14:44:40 +0800

    Add doc and OR-tools base on DG

---
+++ src/algorithm-service/CustomSolver/src/main/java/wzy/jsp/simplest/demo/domain/ortools/DisjunctiveBinaryVariable.java
@@ -0,0 +1,11 @@
+package wzy.jsp.simplest.demo.domain.ortools.test;
+
+import com.google.ortools.sat.IntVar;
+
+import java.util.UUID;
+
+public class DisjunctiveBinaryVariable{
+    public String taskId_o;
+    public String taskId_p;
+    public IntVar x;
+}
